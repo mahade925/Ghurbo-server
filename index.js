2a9e3de923d4551fdb9abe591f8449f66d42c280
mahade925 <learnwithhasan2@gmail.com>
2021-10-31 00:55:50 +0600

    something added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors');
 
@@ -18,13 +19,38 @@ async function run() {
         await client.connect();
         const database = client.db('tourist_agency');
         const serviceCollection = database.collection('services');
+        const orderCollection = database.collection('orders');
 
         // GET Services API
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        // GET SINGLE SERVICE
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.json(service)
+        });
+
+        // ADD ORDERS API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result)
+            console.log(order);
+        });
+
+        // GET ORDERS API
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
         })
+        
     }finally {
         // await client.close();
     }
